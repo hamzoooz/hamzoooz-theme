3a50c96be173a9c186b380d071f59842df5ded08
hamzoooz <hamzoooz123@gmail.com>
2023-02-02 01:06:25 +0200

    add /assets folder and move all /assets files to it

--- functions.php
+++ functions.php
@@ -126,7 +126,7 @@ add_action( 'after_setup_theme', 'hamzoooz_setup' );
 	// Register and Enqueue Stylesheet
 		wp_enqueue_style( 'merlin-stylesheet', get_stylesheet_uri(), array(), $theme_version );
         wp_enqueue_style('bootstrap-css',get_template_directory_uri().'/assets/css/bootstrap.min.css');
-        wp_enqueue_style('font-awesome-css',get_template_directory_uri().'/assets/css/all.css');
+        wp_enqueue_style('font-awesome-css',get_template_directory_uri().'/assets/css/fontawesome.min.css');
         wp_enqueue_style('main',get_template_directory_uri().'/assets/css/main.css');
     }
 
@@ -139,7 +139,7 @@ add_action( 'after_setup_theme', 'hamzoooz_setup' );
         wp_deregister_script('jquery'); // to remove old jquery from wordpress
         wp_register_script('jquery', includes_url('/js/jquery/jquery.js') ,false,'', true);// add anew jquery to footer 
         wp_enqueue_script('bootstrap-js',get_template_directory_uri() .'/assets/js/bootstrap.min.js', array('jquery'), false, true );/* last true to put file script in last body becous the default value false */
-        wp_enqueue_script('fontawesome-js',get_template_directory_uri() .'/assets/js/all.js', array(), false, true );
+        wp_enqueue_script('fontawesome-js',get_template_directory_uri() .'/assets/js/fontawesome.min.js', array(), false, true );
         wp_enqueue_script('main-js',get_template_directory_uri() . '/assets/js/main.js',array(),false,true);//array to tell what incluede this fun from libaraly
         wp_enqueue_script('HTML5 Shiv',get_template_directory_uri() . '/assets/js/HTML5 Shiv 3.7.3.js');
         wp_script_add_data('HTML5 Shiv','conditional','lt IE 9');

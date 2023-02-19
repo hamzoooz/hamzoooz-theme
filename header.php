69ecfbb11e1fc6dab12af6235c493bf0f9dee6a8
hamzoooz <hamzoooz123@gmail.com>
2023-02-19 16:47:29 +0200

    add sass

--- header.php
+++ header.php
@@ -13,7 +13,7 @@
 // $theme_options = hamzoooz_theme_options();
 ?>
 <!DOCTYPE html>
-<html <?php language_attributes(); ?>>
+<html <?php language_attributes(); bloginfo('language'); bloginfo('text_direction'); ?>>
 <head>
     <meta charset = "<?php bloginfo("charset"); ?>" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" >
